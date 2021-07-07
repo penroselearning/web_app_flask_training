a06a1c5088975ea9ed64177ee203ff031fe14aaf
Penrose Learning <50510199+penroselearning@users.noreply.github.com>
2021-07-07 13:02:48 +0400

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request
+import json, random
 
 app = Flask(__name__)
 
@@ -73,6 +74,66 @@ def pin_authentication():
             return render_template("pin_user_info.html", user=user)
         else:
             return render_template("pin_user_info.html", error="user not found")
+
+@app.route("/shopping_cart", methods=["POST", "GET"])
+def shopping_cart():
+
+    shopping_cart = {
+        1234 : ["Apples", "Mangoes", "Bananas", "Stawberries", "Avacado"],
+        5322 : ["Bananas", "Stawberries", "Avacado"],
+        1002 : ["Apples", "Mangoes", "Avacado"]
+    }
+
+    if request.method == "GET":
+        return render_template("pin_access_shopping_cart.html")
+
+    elif request.method == "POST":
+        pin = int(request.form['pin'])
+
+        shopping_list = shopping_cart[pin]
+
+        return render_template("shopping_cart.html", shopping_list=shopping_list)
+
+@app.route("/user_shopping_cart", methods=["POST", "GET"])
+def user_shopping_cart():
+
+    if request.method == "GET":
+        return render_template("shopping_cart.html")
+
+    elif request.method == "POST": 
+        
+        item = request.form['item']
+        add_remove = request.form['add_remove']
+
+        if add_remove == "add":
+            shopping_cart.append(item)
+        elif add_remove == "remove":
+            shopping_cart.remove(item)
+
+        return render_template("shopping_cart.html", shopping_list=shopping_list)
+        
+
+@app.route("/lib_management", methods=["POST", "GET"])
+def lib_management():
+
+    with open("static/data/library.json", "r") as file:
+        library = json.load(file)
+
+    if request.method == "GET":
+        return render_template("library_management_system.html", library=library)
+
+    elif request.method == "POST":
+        name_of_book = request.form['name_of_book']
+        name_of_author = request.form['name_of_author']
+
+        randomID = str(random.randint(1, 5000))
+
+        library[randomID] = [name_of_book, name_of_author]
+
+        with open("static/data/library.json", "w") as file:
+            json.dump(library, file, indent=2)
+        
+        return render_template("library_management_system.html", library=library)
 
 if __name__ == '__main__':
     app.run(debug=True)
